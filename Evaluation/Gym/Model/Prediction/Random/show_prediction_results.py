19c7d7bf1db008384e448e93df6d0912c8afaaf7
Roman Parak <Roman.Parak@outlook.com>
2024-01-02 15:17:27 +0100

    improvements of the repo.

---
+++ Evaluation/Gym/Model/Prediction/Random/show_prediction_results.py
@@ -0,0 +1,33 @@
+# System (Default)
+import sys
+#   Add access if it is not in the system path.
+if '../../../../' + 'src' not in sys.path:
+    sys.path.append('../../../../' + 'src')
+# OS (Operating system interfaces)
+import os
+# Numpy (Array computing) [pip3 install numpy]
+import numpy as np
+# SciencePlots (Matplotlib styles for scientific plotting) [pip3 install SciencePlots]
+import scienceplots
+# Matplotlib (Visualization) [pip3 install matplotlib]
+import matplotlib.pyplot as plt
+# Custom Lib.:
+#   Robotics Library for Everyone (RoLE)
+#       ../RoLE/Parameters/Robot
+import RoLE.Parameters.Robot as Parameters
+
+"""
+Description:
+    Initialization of constants.
+"""
+# Set the structure of the main parameters of the robot.
+CONST_ROBOT_TYPE = Parameters.Universal_Robots_UR3_Str
+# Locate the path to the project folder.
+CONST_PROJECT_FOLDER = os.getcwd().split('PyBullet_Industrial_Robotics_Gym')[0] + 'PyBullet_Industrial_Robotics_Gym'
+
+def main():
+    # Initialization of the structure of the main parameters of the robot.
+    Robot_Str = CONST_ROBOT_TYPE
+
+if __name__ == '__main__':
+    sys.exit(main())

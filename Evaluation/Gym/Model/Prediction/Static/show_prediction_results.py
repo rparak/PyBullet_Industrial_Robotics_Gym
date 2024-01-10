ed67810783f96bf20817cc469eccb6ed962f8f0e
Roman Parak <59449320+rparak@users.noreply.github.com>
2024-01-10 09:02:00 +0100

    Update show_prediction_results.py

--- Evaluation/Gym/Model/Prediction/Static/show_prediction_results.py
+++ Evaluation/Gym/Model/Prediction/Static/show_prediction_results.py
@@ -36,7 +36,7 @@ CONST_ROBOT_TYPE = Parameters.Universal_Robots_UR3_Str
 #       The mode called "Default" demonstrates an environment without a collision object.
 #   'Collision-Free': 
 #       The mode called "Collision-Free" demonstrates an environment with a collision object.
-CONST_ENV_MODE = 'Default'
+CONST_ENV_MODE = 'Collision-Free'
 # The name of the reinforcement learning algorithm. 
 #   Deep Deterministic Policy Gradient (DDPG)
 #       CONST_ALGORITHM = 'DDPG' or 'DDPG_HER'
@@ -51,6 +51,8 @@ CONST_ALGORITHM = 'DDPG'
 #   'method': The method to be used to select the parameters of the knot vector. 
 #               method = 'Uniformly-Spaced', 'Chord-Length' or 'Centripetal'.
 CONST_B_SPLINE = {'n': 3, 'N': 100, 'method': 'Chord-Length'}
+# Number of optimized control points.
+CONST_NUM_OF_OPT_POINTS = 4
 # Locate the path to the project folder.
 CONST_PROJECT_FOLDER = os.getcwd().split('PyBullet_Industrial_Robotics_Gym')[0] + 'PyBullet_Industrial_Robotics_Gym'
 
@@ -97,52 +99,56 @@ def main():
     L = S_Cls.Get_Arc_Length()
 
     # Initialization of a specific class to work with B-Spline curves from 
-    # noisy points.
+    # control points.
     S_Cls_2 = B_Spline.B_Spline_Cls(CONST_B_SPLINE['n'], CONST_B_SPLINE['method'], data, 
                                     CONST_B_SPLINE['N'])
     
-    # Get optimized control points from noisy points.
-    S_Cls_optimized = S_Cls_2.Optimize_Control_Points(4)
+    # Get optimized control points.
+    S_Cls_optimized = S_Cls_2.Optimize_Control_Points(CONST_NUM_OF_OPT_POINTS)
 
     # Interpolation of the parametric B-Spline curve from the optimized control points.
     S_optimized = S_Cls_optimized.Interpolate()
+
+    # Obtain the arc length L(x) of the optimized parametric curve.
+    L_optimized = S_Cls_optimized.Get_Arc_Length()
 
     # Display informations.
     print('[INFO] Absolute Position Error (APE):')
     print(f'[INFO] >> e_p(t) = {np.linalg.norm(data[-1] - T.p.all(), axis=-1).astype(np.float32):.5f} in meters')
     print('[INFO] Arc length L(t):')
     print(f'[INFO] >> L = {L:.5f} in meters')
+    print(f'[INFO] >> L_optimized = {L_optimized:.5f} in meters')
 
     # Set the parameters for the scientific style.
     plt.style.use('science')
 
     # Get the normalized time.
     t_hat = np.linspace(0.0, 1.0, data[:, 0].size)
-    t_hat2 = np.linspace(0.0, 1.0, S_Cls_optimized.P[:, 0].size)
+    t_hat_optimized = np.linspace(0.0, 1.0, S_Cls_optimized.P[:, 0].size)
 
     # Display TCP(Tool Center Point) parameters.
     y_label = [r'$x(\hat{t})$ in meters', r'$y(\hat{t})$ in meters', r'$z(\hat{t})$ in meters']
-    for i, (data_i, data_S_i, data_S2_i, data_2_i, p_i) in enumerate(zip(S_Cls.P.T, S.T, S_optimized.T, S_Cls_optimized.P.T, T.p.all())):
+    for i, (P_i, S_i, O_P_i, O_S_i, p_i) in enumerate(zip(S_Cls.P.T, S.T, S_Cls_optimized.P.T, S_optimized.T, T.p.all())):
         # Create a figure.
         _, ax = plt.subplots()
 
         # Visualization of relevant structures.
         ax.scatter(t_hat[-1], p_i, c='#84b070', s=100.0, linewidths=1, label=f'Target')
-        ax.plot(t_hat, data_i, 'o--', color='#d0d0d0', linewidth=1.0, markersize=6.0, 
-                markeredgewidth=3.0, markerfacecolor='#ffffff', label=f'Predicted Control Points')
-        ax.plot(S_Cls.x, data_S_i, '-', color='#ffcb99', linewidth=1.0, label=f'B-Spline (n = {S_Cls.n}, N = {S_Cls.N}, L = {L:.03})')
-        ax.plot(t_hat2, data_2_i, 'o--', color='#adceeb', linewidth=1.0, markersize=6.0, 
-                markeredgewidth=3.0, markerfacecolor='#ffffff', label=f'Predicted Control Points')
-        ax.plot(S_Cls.x, data_S2_i, '-', color='#adceeb', linewidth=1.0, label=f'B-Spline ....')
-        ax.plot([t_hat[-1]] * 2, [data_i[-1], p_i], '.-', color='#e06666', linewidth=1.0, label=f'$\Delta$e = {np.abs(data_i[-1] - p_i):.5f} in meters')
+        ax.plot(t_hat, P_i, 'o--', color='#d0d0d0', linewidth=1.0, markersize=6.0, 
+                markeredgewidth=3.0, markerfacecolor='#ffffff', label=f'Control Points')
+        ax.plot(S_Cls.x, S_i, '-', color='#d0d0d0', linewidth=1.0, label=f'B-Spline (L = {L:.03})')
+        ax.plot(t_hat_optimized, O_P_i, 'o--', color='#ffcb99', linewidth=1.0, markersize=6.0, 
+                markeredgewidth=3.0, markerfacecolor='#ffffff', label=f'Optimized Control Points using the Least-Squares Method')
+        ax.plot(S_Cls.x, O_S_i, '-', color='#ffcb99', linewidth=1.0, label=f'Optimized B-Spline (L = {L_optimized:.03})')
+        ax.plot([t_hat[-1]] * 2, [P_i[-1], p_i], '.-', color='#e06666', linewidth=1.0, label=f'$\Delta$e = {np.abs(P_i[-1] - p_i):.5f} in meters')
 
         # Set parameters of the graph (plot).
         #   Set the x ticks.
         ax.set_xticks(np.arange(np.min(t_hat) - 0.1, np.max(t_hat) + 0.1, 0.1))
         #   Set the y ticks.
-        tick_y_tmp = (np.max(data_i) - np.min(data_i))/10.0
+        tick_y_tmp = (np.max(O_P_i) - np.min(O_P_i))/10.0
         tick_y = tick_y_tmp if tick_y_tmp != 0.0 else 0.1
-        ax.set_yticks(np.arange(np.min(data_i) - tick_y, np.max(data_i) + tick_y, tick_y))
+        ax.set_yticks(np.arange(np.min(O_P_i) - tick_y, np.max(O_P_i) + tick_y, tick_y))
         #   Label.
         ax.set_xlabel(r'Normalized time $\hat{t}$ in the range of [0.0, 1.0]', fontsize=15, labelpad=10)
         ax.set_ylabel(f'{y_label[i]}', fontsize=15, labelpad=10) 

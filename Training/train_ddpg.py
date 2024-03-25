170ff9322d42883c9697f4e2541b1b3b2d380bb4
Roman Parak <59449320+rparak@users.noreply.github.com>
2024-03-25 11:27:17 +0100

    improvements of the repo.

--- Training/train_ddpg.py
+++ Training/train_ddpg.py
@@ -43,13 +43,13 @@ Description:
     Initialization of constants.
 """
 # Set the structure of the main parameters of the robot.
-CONST_ROBOT_TYPE = Parameters.ABB_IRB_14000_R_Str
+CONST_ROBOT_TYPE = Parameters.Universal_Robots_UR3_Str
 # The name of the environment mode.
 #   'Default': 
 #       The mode called "Default" demonstrates an environment without a collision object.
 #   'Collision-Free': 
 #       The mode called "Collision-Free" demonstrates an environment with a collision object.
-CONST_ENV_MODE = 'Collision-Free'
+CONST_ENV_MODE = 'Default'
 # The name of the reinforcement learning algorithm. 
 #   'DDPG':
 #       Deep Deterministic Policy Gradient (DDPG) combines the trick for DQN with the deterministic 
